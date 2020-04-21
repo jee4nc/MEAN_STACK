08c2321696bb47343a4ca262fff6d8edf8032467
jean <jeanc.aldea1@gmail.com>
2020-04-20 23:12:08 -0400

    Angular_service

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {TasksService} from './services/tasks.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TasksService], // Se importa acá, raro pero wrv
   bootstrap: [AppComponent]
 })
 export class AppModule { }
